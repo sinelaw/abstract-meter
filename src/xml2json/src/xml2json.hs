c244a96d255e854e240d372ca6677726666d0f1c
Noam Lewis <lenoam@gmail.com>
2012-10-17 23:15:55 -0400

    xml2json: added conversion of text (leaves)

--- src/xml2json/src/xml2json.hs
+++ src/xml2json/src/xml2json.hs
@@ -2,11 +2,12 @@ module Main
 where
  
 import Text.XML.HXT.Core
+import qualified Text.XML.HXT.DOM.XmlNode as XN
 import Text.XML.HXT.Curl -- use libcurl for HTTP access
                          -- only necessary when reading http://...
 import qualified Data.Aeson as Aeson
 import qualified Data.HashMap.Strict as M
-import Data.Hashable (Hashable)
+import Data.Hashable (Hashable, hash)
 import Data.Tree.NTree.TypeDefs 
 import System.Environment
 import qualified Data.Vector as Vector
@@ -21,18 +22,25 @@ import qualified Data.ByteString.Lazy.Char8 as BS
 --xpJSON = xpElem "
   
 main :: IO ()
-main
-    = do
-      [src] <- getArgs
-      [rootElem] <- runX ( readDocument [withValidate no
-                                      ,withCurl []
-                                      ] src
-                         >>> getChildren 
-                         >>> isElem
-                       )
-      BS.putStr . Aeson.encode $ xmlTreeToJSON rootElem
-      return ()
+main = do
+       [src] <- getArgs
+       [rootElem] <- runX ( readDocument [withValidate no
+                                         ,withCurl []
+                                         ] src
+                            >>> getChildren 
+                            >>> isElem
+                          )
+       BS.putStr . Aeson.encode . wrapRoot $ xmlTreeToJSON rootElem
+       return ()
       
+data JSValueName = Text | Tag String | Attr String
+     deriving (Eq, Ord, Show)
+
+instance Hashable JSValueName where
+  hash Text     = 0
+  hash (Tag s)  = hash s
+  hash (Attr s) = hash s
+  
 concatMapValues :: (Eq k, Hashable k) => [M.HashMap k v] -> M.HashMap k [v] 
 concatMapValues = foldr (M.unionWith (++) . M.map (:[])) M.empty 
 
@@ -43,42 +51,52 @@ fromObject _                     = Nothing
 getAttrVals :: XmlTree -> [(String, String)]
 getAttrVals node = runLA (getAttrl >>> getName &&& (getChildren >>> getText)) node
 
-xmlTreeToJSON :: XmlTree -> Maybe (T.Text, Aeson.Value)
-xmlTreeToJSON node@(NTree (XTag qName attrs) children) = Just (T.pack (localPart qName), 
-                                                          Aeson.Object objMap)
-  where objMap = arrayValuesToJSONArrays . concatMapValues -- unify into a single object, 
-                                                           -- grouping into arrays by pair name
-               . map (uncurry M.singleton)    -- convert pairs to json objects
-               . (++) attrVals
-               . map fromJust . filter isJust -- filter out the nothings (unconvertable nodes)
-               $ map xmlTreeToJSON children  -- convert xml nodes to (name, json value) pairs
+arrayValuesToJSONArrays :: (Eq k, Hashable k) => M.HashMap k [Aeson.Value] -> M.HashMap k Aeson.Value
+arrayValuesToJSONArrays = M.map fromJust
+                        . M.filter isJust
+                        . M.map (\v -> case v of 
+                                    []  -> Nothing -- will be discarded
+                                    [x] -> Just x  -- don't store as array, just a single value
+                                    xss -> Just $ Aeson.Array . Vector.fromList $ xss) -- arrays with more than one element are kept
 
-        arrayValuesToJSONArrays :: (Eq k, Hashable k) => M.HashMap k [Aeson.Value] -> M.HashMap k Aeson.Value
-        arrayValuesToJSONArrays = M.map fromJust
-                                . M.filter isJust
-                                . M.map (\v -> case v of 
-                                            []  -> Nothing -- will be discarded
-                                            [x] -> Just x  -- don't store as array, just a single value
-                                            xss -> Just $ Aeson.Array . Vector.fromList $ xss) -- arrays with more than one element are kept
-                                
-        -- apply on object internal maps, ignore non-object json values (don't apply on arrays)
-        fmapObj :: (Aeson.Object -> Aeson.Object) -> Aeson.Value -> Aeson.Value
-        fmapObj f (Aeson.Object objMap) = Aeson.Object (f objMap)
-        fmapObj f val                   = val 
+-- apply on object internal maps, ignore non-object json values (don't apply on arrays)
+fmapObj :: (Aeson.Object -> Aeson.Object) -> Aeson.Value -> Aeson.Value
+fmapObj f (Aeson.Object objMap) = Aeson.Object (f objMap)
+fmapObj f val                   = val 
+
+packJSValueName :: JSValueName -> T.Text 
+packJSValueName Text = T.pack "value"
+packJSValueName (Attr x) = T.pack x
+packJSValueName (Tag x)  = T.pack x
+
+mapKeys :: (Eq k1, Eq k2, Hashable k1, Hashable k2) => (k1 -> k2) -> M.HashMap k1 v -> M.HashMap k2 v
+mapKeys f = M.foldrWithKey (\k v -> M.insert (f k) v) M.empty
+
+wrapRoot :: Maybe (JSValueName, Aeson.Value) -> Aeson.Value
+wrapRoot Nothing       = Aeson.Null
+wrapRoot (Just (a, b)) = Aeson.object [(packJSValueName a, b)] 
+
+xmlTreeToJSON :: XmlTree -> Maybe (JSValueName, Aeson.Value)
+xmlTreeToJSON node@(NTree (XTag qName attrs) children) 
+  = Just (Tag (localPart qName), 
+          Aeson.Object objMap)
+  where objMap = mapKeys packJSValueName 
+               . arrayValuesToJSONArrays    -- unify into a single map,
+               . concatMapValues            -- grouping into arrays by pair name
+               . map (uncurry M.singleton)  -- convert pairs to maps
+               . (++) attrVals       
+               . map fromJust               -- unpack the Just values
+               . filter isJust              -- filter out the empty values (unconvertable nodes)
+               $ map xmlTreeToJSON children -- convert xml nodes to (Maybe QName, Maybe Aeson.Value) pairs
         
-        attrVals = map (\(name, val) -> (T.pack name, Aeson.String (T.pack val))) $ getAttrVals node        
+        attrVals = map (\(name, val) -> (Attr name, Aeson.String (T.pack val))) $ getAttrVals node        
 
+xmlTreeToJSON (NTree (XText str) _) = if (T.empty == text) 
+                                      then Nothing
+                                      else Just $ (Text, Aeson.String text)
+  where text = T.strip . T.pack $ str
 xmlTreeToJSON (NTree _ children) = Nothing 
                 
-
-          
-  --                          foldr addChild singleton children
-  -- where
-  --   addChild :: (NTree XmlTree) -> Aeson.Object -> Aeson.Object
-  --   addChild tree@(NTree (XTag childName attrs) _) childLists = 
-  --     case (HMap.lookup (== child) ) of
-  --       Nothing                -> (xmlTreeToJSON tree) : childLists
-  --       Just existingChildName -> 
 
 
   
